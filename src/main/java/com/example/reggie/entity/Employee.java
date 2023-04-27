f5b39eda527f0ff8e69226f539dce68134b16d78
simeis147 <cpdong7@gmail.com>
2023-04-27 15:58:32 +0800

    update

--- src/main/java/com/example/reggie/entity/Employee.java
+++ src/main/java/com/example/reggie/entity/Employee.java
@@ -2,7 +2,10 @@ package com.example.reggie.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -10,6 +13,8 @@ import java.time.LocalDateTime;
  * @author Dongcp
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
