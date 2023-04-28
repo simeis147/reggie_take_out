ecc9a3a7ce36a6a6f410a573ecfc7f2216c632d2
simeis147 <cpdong7@gmail.com>
2023-04-28 14:11:54 +0800

    update

--- src/main/java/com/example/reggie/ReggieApplication.java
+++ src/main/java/com/example/reggie/ReggieApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * @author 董成鹏
+ */
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
-        log.info("Project success");
+        log.info("Project success ! ");
     }
 }
