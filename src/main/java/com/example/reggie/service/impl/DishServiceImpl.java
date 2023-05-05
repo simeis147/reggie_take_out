147b28972e7fb1ffeec742c50aa3bc542a1a5ea4
simeis147 <cpdong7@gmail.com>
2023-05-05 15:45:28 +0800

    菜单新增和分页查询

--- src/main/java/com/example/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/example/reggie/service/impl/DishServiceImpl.java
@@ -30,7 +30,6 @@ public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements Di
     public void saveWithFlavor(DishDto dishDto) {
         //保存菜品的基本信息到菜品表dish
         this.save(dishDto);
-
         Long dishId = dishDto.getId();
         //菜品口味
         List<DishFlavor> flavors = dishDto.getFlavors();
