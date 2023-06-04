3e4c90ee258ac732060344b6792bf6343167f7d4
simeis147 <cpdong7@gmail.com>
2023-06-04 14:25:43 +0800

    新增购物车功能

---
+++ src/main/java/org/example/reggie/controller/ShoppingCartController.java
@@ -0,0 +1,92 @@
+package org.example.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.example.reggie.common.BaseContext;
+import org.example.reggie.common.R;
+import org.example.reggie.entity.ShoppingCart;
+import org.example.reggie.service.CategoryService;
+import org.example.reggie.service.ShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/shoppingCart")
+@Slf4j
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 购物车的添加功能
+     * @param shoppingCart
+     * @return
+     */
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart){
+        log.info("购物车数据： {}",shoppingCart);
+
+        //设置用户id，指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        Long dishId = shoppingCart.getDishId();
+
+        LambdaQueryWrapper<ShoppingCart> qw = new LambdaQueryWrapper<>();
+        qw.eq(ShoppingCart::getId,currentId);
+
+        if (dishId != null){
+            qw.eq(ShoppingCart::getDishId,dishId);
+        }else {
+            qw.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+        }
+
+        //查询当前菜品或者套餐是否在购物车中
+        //SQL:select * from shopping_cart where user_id = ? and dish_id/setmeal_id = ?
+        ShoppingCart one = shoppingCartService.getOne(qw);
+
+        if (one != null){
+            Integer number = one.getNumber();
+            one.setNumber(number + 1);
+            shoppingCartService.updateById(one);
+        }else {
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartService.save(shoppingCart);
+            one = shoppingCart;
+        }
+        return R.success(one);
+    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> listR(){
+        log.info("查看购物车......");
+
+        LambdaQueryWrapper<ShoppingCart> qw = new LambdaQueryWrapper<>();
+        qw.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+        qw.orderByDesc(ShoppingCart::getCreateTime);
+
+        List<ShoppingCart> list = shoppingCartService.list(qw);
+        return R.success(list);
+    }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    @DeleteMapping("/clean")
+    public R<String> clean(){
+        LambdaQueryWrapper<ShoppingCart> qw = new LambdaQueryWrapper<>();
+        qw.eq(ShoppingCart::getId,BaseContext.getCurrentId());
+
+        shoppingCartService.remove(qw);
+        return R.success("清空购物车成功！");
+    }
+}
---
+++ src/main/java/org/example/reggie/service/ShoppingCartService.java
@@ -0,0 +1,7 @@
+package org.example.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.example.reggie.entity.ShoppingCart;
+
+public interface ShoppingCartService extends IService<ShoppingCart> {
+}
